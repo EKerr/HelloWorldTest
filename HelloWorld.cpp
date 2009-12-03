03050711cb4957ca7e8607f785e6393eb1744648
Emmett <ep.kerr@ulster.ac.uk>
2009-12-03 11:25:22 +0000

    This is the second test for GIT
    Added a second line of text output!

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -6,6 +6,7 @@ int main ()
   cout << "Hello World! \n\n";
   cout << "I'm a C++ Program \n\n";
   cout << ,"This is the first change to test GIT" << endl;
+  cout << ,"This is the second change to test GIT" << endl;
   return 0;
 }
 
